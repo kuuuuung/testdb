80120d1b078109f7fa729e6961a1ed5ad53e851d
kungnang <s6102041520025@email.kmutnb.ac.th>
2021-05-18 20:38:59 +0700

    first commit

---
+++ index.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="BASICDB.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Username
+            <asp:TextBox ID="txtUser" runat="server"></asp:TextBox>
+&nbsp;Password
+            <asp:TextBox ID="txtPass" runat="server"></asp:TextBox>
+&nbsp;<asp:Button ID="cmdAdd" runat="server" OnClick="Button1_Click" Text="ADD" />
+            <br />
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BASICDB
+{
+    public partial class index : System.Web.UI.Page
+    {
+        DB1Entities edm = new DB1Entities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if((txtUser.Text.Trim() != "") && (txtPass.Text.Trim() != ""))
+            {
+                user u = new user();
+                u.username = txtUser.Text.Trim();
+                u.pass = txtPass.Text.Trim();
+
+                if(edm.Database.Connection.State == System.Data.ConnectionState.Closed)
+                {
+                    edm.Database.Connection.Open();
+
+                }
+                edm.users.Add(u);
+                edm.SaveChanges();
+
+                txtPass.Text = "";
+                txtUser.Text = "";
+                showUser();
+            }
+
+        }
+        private void showUser()
+        {
+            var us = from p in edm.users
+                     orderby p.id descending
+                     select new
+                     {
+                         id = p.id,
+                         username = p.username
+                     };
+            if (us.Count() > 0)
+            {
+                GridView1.DataSource = us.ToList();
+                GridView1.DataBind();
+            }
+        }
+    }
+}
